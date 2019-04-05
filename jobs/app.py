711351cad0fc11b6d5ed5c972ee4a5f5bef08e5b
I355519 <venkateswarlu.inkollu@sap.com>
2019-04-05 14:52:12 +0530

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -14,7 +14,7 @@ def open_connection():
 def execute_sql(sql,values=(), commit=False ,single=False):
     connection=open_connection()
     cursor=connection.execute(sql,values)
-    if commit==True:
+    if commit == True:
         results=connection.commit()
     else:
         results=cursor.fetchone() if single else cursor.fetchall()
